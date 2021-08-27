6fb221404cce5e0e61ec00a1fb58562af14f3ee4
Brezus <73348763+Brezus@users.noreply.github.com>
2021-08-27 04:04:30 -0400

    many a changes

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ function hamburgerClick() {
   let hamburger = document.getElementById("hamburger-icon");
   let hiddenNav = document.getElementById("nav-ul");
   let bgDisappear = document.getElementById("first-section");
-  bgDisappear.classList.toggle("clicked");
+  bgDisappear.classList.toggle("first-section-dark-gradient");
+  hamburger.classList.toggle("change");
   if (hiddenNav.style.display === "flex") {
     hiddenNav.style.display = "none";
   } else {
