192e2cc7b462e616f5a0272e0ee8a137f6100b1f
Brezus <73348763+Brezus@users.noreply.github.com>
2021-11-05 19:23:46 -0400

    yep

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+gsap.to(".hamburger", { duration: 0.5, opacity: 1 });
+
+gsap.to(
+  ".logo-svg",
+  { duration: 1, x: 0, opacity: 1, ease: "back.out(1.4)" },
+  "+=0.1"
+);
+
 function hamburgerClick() {
   let hamburger = document.getElementById("hamburger-icon");
   let hiddenNav = document.getElementById("nav-ul");
   let bgDisappear = document.getElementById("first-section");
   bgDisappear.classList.toggle("first-section-dark-gradient");
-  console.log(hiddenNav.value);
   hamburger.classList.toggle("change");
   if (hiddenNav.style.display === "flex") {
     hiddenNav.style.display = "none";
